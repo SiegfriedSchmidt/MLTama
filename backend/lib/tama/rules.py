014fb2ed04e489a3fa0b0e831f0ad32820e1c4f5
Matvei <matveiyan@yandex.ru>
2025-04-01 00:28:37 +0300

    adding captures (almost completed) and usual moves

--- backend/lib/tama/rules.py
+++ backend/lib/tama/rules.py
@@ -4,9 +4,10 @@ from numba import njit, types, typeof
 from lib.tama.fen import fen_to_field
 
 mcd = np.array([[0, -1], [-1, 0], [0, 1]], dtype=np.int8)  # men directions with capture
-md = np.array([[-1, -1], [-1, 1]], dtype=np.int8)  # men directions without capture
+md = np.array([[0, -1], [-1, -1], [-1, 0], [-1, 1], [0, 1]], dtype=np.int8)  # men directions without capture
 kcd = np.array([[0, -1], [-1, 0], [0, 1], [1, 0]], dtype=np.int8)  # king directions with capture
-kd = np.array([[-1, -1], [-1, 1], [1, 1], [1, -1]], dtype=np.int8)  # king directions without capture
+kd = np.array([[0, -1], [-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1]],
+              dtype=np.int8)  # king directions without capture
 
 '''
 1 1
@@ -21,13 +22,7 @@ def on_board(row, col):
 
 
 @njit()
-def find_possible_capture_for_men(field, row, col, capture, capture_low, capture_high, capture_len, max_capture):
-    reverted_piece, reverted_capture_idx = 0, capture_high
-    max_capture = max(max_capture, capture_len)
-    if capture_high != 0:
-        reverted_piece = field[capture[capture_high - 1, 0], capture[capture_high - 1, 1]]
-        field[capture[capture_high - 1, 0], capture[capture_high - 1, 1]] = 0
-
+def find_capture_for_piece(field, row, col):
     for i in range(mcd.shape[0]):
         row2 = row + mcd[i, 0]
         col2 = col + mcd[i, 1]
@@ -36,47 +31,129 @@ def find_possible_capture_for_men(field, row, col, capture, capture_low, capture
         if field[row2, col2] == -1:
             row3 = row2 + mcd[i, 0]
             col3 = col2 + mcd[i, 1]
+            if not on_board(row3, col3):
+                continue
             if field[row3, col3] == 0:
-                capture[capture_high] = row2, col2, row3, col3
-                capture_low, capture_high, max_capture = find_possible_capture_for_men(
-                    field, row3, col3, capture, capture_low, capture_high + 1, capture_len + 1, max_capture
-                )
+                yield row2, col2, row3, col3
 
-    if reverted_piece:
-        field[capture[reverted_capture_idx, 0], capture[reverted_capture_idx, 1]] = reverted_piece
 
-    return capture_low, capture_high, max_capture
+# TODO: men promoted to king and continue capture
+@njit()
+def find_capture_for_piece_with_capture(field, row, col):
+    for row2, col2, row3, col3 in find_capture_for_piece(field, row, col):
+        captured_piece = field[row2, col2]
+        field[row2, col2] = 0
+        yield row2, col2, row3, col3
+        field[row2, col2] = captured_piece
+
+
+@njit()
+def find_capture_max_depth(field, row, col, depth):
+    max_depth = depth
+
+    for row2, col2, row3, col3 in find_capture_for_piece_with_capture(field, row, col):
+        max_depth = max(max_depth, find_capture_max_depth(field, row3, col3, depth + 1))
+
+    return max_depth
+
+
+@njit()
+def find_field_capture_max_depth(field):
+    max_capture = 0
+    for row in range(field.shape[0]):
+        for col in range(field.shape[1]):
+            if field[row, col] == 1:
+                max_capture = max(max_capture, find_capture_max_depth(field, row, col, 0))
+
+    return max_capture
+
+
+@njit()
+def find_possible_capture_for_men(field, row, col, depth, max_depth, piece_idx, capture):
+    if depth == max_depth:
+        return 1
+
+    piece_offset = piece_idx
+    for row2, col2, row3, col3 in find_capture_for_piece_with_capture(field, row, col):
+        cnt = find_possible_capture_for_men(field, row3, col3, depth + 1, max_depth, piece_idx, capture)
+        for i in range(cnt):
+            capture[piece_idx + i, depth + 1] = row2, col2, row3, col3
+
+        piece_idx += cnt
+
+    return piece_idx - piece_offset
+
+
+@njit()
+def find_field_possible_capture(field, max_capture, capture):
+    piece_idx = 0
+    for row in range(field.shape[0]):
+        for col in range(field.shape[1]):
+            if field[row, col] == 1:
+                cnt = find_possible_capture_for_men(field, row, col, 0, max_capture, piece_idx, capture)
+                for i in range(cnt):
+                    capture[piece_idx + i, 0] = row, col, max_capture, 0
+
+                piece_idx += cnt
+
+    return piece_idx
+
+
+@njit()
+def find_field_possible_moves(field, non_capture):
+    piece_idx = 0
+    for row in range(field.shape[0]):
+        for col in range(field.shape[1]):
+            if field[row, col] == 1:
+                for i in range(md.shape[0]):
+                    row2 = row + md[i, 0]
+                    col2 = col + md[i, 1]
+                    if not on_board(row2, col2) or field[row2, col2] != 0:
+                        continue
+                    non_capture[piece_idx] = row, col, row2, col2, 0
+                    piece_idx += 1
+
+    return piece_idx
 
 
 @njit()
 def get_possible_moves(input_field: np.ndarray, side: int):
     field = np.copy(input_field) * side
 
-    capture = np.zeros((100, 4), dtype=np.int8)  # TODO: change to empty
-    capture_low, capture_high, max_capture = 0, 0, 0
-    for row in range(field.shape[0]):
-        for col in range(field.shape[1]):
-            if field[row, col] == 1:
-                capture_low, capture_high, max_capture = find_possible_capture_for_men(
-                    field, row, col, capture, capture_low, capture_high, 0, max_capture
-                )
+    # dimensions: 0 - idx of sequence, 1 - move idx in sequence, 2 - move (captured_row, captured_col, new_row, new_col)
+    # first "move" in every sequence (row, col, capture_depth, promoted)
+    capture = np.zeros((20, 16, 4), dtype=np.int8)
+    captured_idx = 0
+    # dimensions: 0 - idx of move, 1 - move (from_row, from_col, to_row, to_col, promoted)
+    non_capture = np.zeros((168, 5), dtype=np.int8)
+    non_capture_idx = 0
 
-    print(capture_low, capture_high, max_capture)
-    print(capture[capture_low:capture_high])
+    max_capture = find_field_capture_max_depth(field)
+    if max_capture > 0:
+        captured_idx = find_field_possible_capture(field, max_capture, capture)
+        for i in range(captured_idx):
+            print(capture[i, 0: capture[i, 0, 2] + 1])
+    else:
+        non_capture_idx = find_field_possible_moves(field, non_capture)
+        for i in range(non_capture_idx):
+            print(non_capture[i])
+
+    return capture, captured_idx, non_capture, non_capture_idx
 
 
 if __name__ == '__main__':
-    # start_field = fen_to_field('8/wwwwwwww/wwwwwwww/8/8/bbbbbbbb/bbbbbbbb/8 w')
-    start_field = np.array([
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [-1, -1, -1, -1, -1, -1, -1, -1],
-        [-1, -1, -1, -1, -1, -1, -1, -1],
-        [0, 0, 0, 0, -1, 0, 0, 0],
-        [-1, -1, 0, 0, 0, -1, 0, 0],
-        [1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-    ], dtype=np.int8)
+    start_field = fen_to_field('8/wwwwwwww/wwwwwwww/8/8/bbbbbbbb/bbbbbbbb/8 w')
+    # start_field = np.array([
+    #     [0, 0, 0, 0, 0, 0, 0, 0],
+    #     [-1, -1, -1, -1, -1, -1, -1, -1],
+    #     [-1, -1, -1, -1, -1, -1, -1, -1],
+    #     [0, 0, -1, 0, -1, 0, 0, 0],
+    #     [-1, -1, 0, 0, 0, -1, 0, 0],
+    #     [1, 1, 1, 1, 1, 1, 1, 1],
+    #     [1, 1, 1, 1, 1, 1, 1, 1],
+    #     [0, 0, 0, 0, 0, 0, 0, 0],
+    # ], dtype=np.int8)
+    print(start_field)
     get_possible_moves(start_field, 1)
 
 '''
