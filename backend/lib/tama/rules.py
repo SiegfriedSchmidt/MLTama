e1ee479bbdfa592559f72d7f6bd29b42993bf496
Matvei <matveiyan@yandex.ru>
2025-04-01 15:23:08 +0300

    adding promoted flag

--- backend/lib/tama/rules.py
+++ backend/lib/tama/rules.py
@@ -9,12 +9,6 @@ kcd = np.array([[0, -1], [-1, 0], [0, 1], [1, 0]], dtype=np.int8)  # king direct
 kd = np.array([[0, -1], [-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1]],
               dtype=np.int8)  # king directions without capture
 
-'''
-1 1
-
-
-'''
-
 
 @njit()
 def on_board(row, col):
@@ -22,60 +16,79 @@ def on_board(row, col):
 
 
 @njit()
-def find_capture_for_piece(field, row, col):
-    for i in range(mcd.shape[0]):
-        row2 = row + mcd[i, 0]
-        col2 = col + mcd[i, 1]
-        if not on_board(row2, col2):
-            continue
-        if field[row2, col2] == -1:
-            row3 = row2 + mcd[i, 0]
-            col3 = col2 + mcd[i, 1]
-            if not on_board(row3, col3):
+def find_capture_for_piece(field, side, row, col):
+    if field[row, col] * side == 1:
+        for i in range(mcd.shape[0]):
+            row2 = row + mcd[i, 0]
+            col2 = col + mcd[i, 1]
+            if not on_board(row2, col2):
                 continue
-            if field[row3, col3] == 0:
-                yield row2, col2, row3, col3
+            if field[row2, col2] * side == -1:
+                row3 = row2 + mcd[i, 0]
+                col3 = col2 + mcd[i, 1]
+                if not on_board(row3, col3):
+                    continue
+                if field[row3, col3] == 0:
+                    yield row2, col2, row3, col3, (side == 1 and row3 == 0) or (side == -1 and row3 == 7)
+
+
+@njit()
+def find_moves_for_piece(field, side, row, col):
+    if field[row, col] * side == 1:
+        for i in range(md.shape[0]):
+            row2 = row + md[i, 0]
+            col2 = col + md[i, 1]
+            if not on_board(row2, col2) or field[row2, col2] != 0:
+                continue
+
+            yield row2, col2
 
 
 # TODO: men promoted to king and continue capture
 @njit()
-def find_capture_for_piece_with_capture(field, row, col):
-    for row2, col2, row3, col3 in find_capture_for_piece(field, row, col):
+def find_capture_for_piece_with_capture(field, side, row, col):
+    for row2, col2, row3, col3, promoted in find_capture_for_piece(field, side, row, col):
         captured_piece = field[row2, col2]
+        piece = field[row, col]
+
+        field[row3, col3] = (2 if promoted else 1) * side
+        field[row, col] = 0
         field[row2, col2] = 0
-        yield row2, col2, row3, col3
+
+        yield row2, col2, row3, col3, promoted
+
         field[row2, col2] = captured_piece
+        field[row, col] = piece
+        field[row3, col3] = 0
 
 
 @njit()
-def find_capture_max_depth(field, row, col, depth):
+def find_capture_max_depth(field, side, row, col, depth):
     max_depth = depth
-
-    for row2, col2, row3, col3 in find_capture_for_piece_with_capture(field, row, col):
-        max_depth = max(max_depth, find_capture_max_depth(field, row3, col3, depth + 1))
+    for _, _, row3, col3, _ in find_capture_for_piece_with_capture(field, side, row, col):
+        max_depth = max(max_depth, find_capture_max_depth(field, side, row3, col3, depth + 1))
 
     return max_depth
 
 
 @njit()
-def find_field_capture_max_depth(field):
+def find_field_capture_max_depth(field, side):
     max_capture = 0
     for row in range(field.shape[0]):
         for col in range(field.shape[1]):
-            if field[row, col] == 1:
-                max_capture = max(max_capture, find_capture_max_depth(field, row, col, 0))
+            max_capture = max(max_capture, find_capture_max_depth(field, side, row, col, 0))
 
     return max_capture
 
 
 @njit()
-def find_possible_capture_for_men(field, row, col, depth, max_depth, piece_idx, capture):
+def find_possible_capture(field, side, row, col, depth, max_depth, piece_idx, capture):
     if depth == max_depth:
         return 1
 
     piece_offset = piece_idx
-    for row2, col2, row3, col3 in find_capture_for_piece_with_capture(field, row, col):
-        cnt = find_possible_capture_for_men(field, row3, col3, depth + 1, max_depth, piece_idx, capture)
+    for row2, col2, row3, col3, promoted in find_capture_for_piece_with_capture(field, side, row, col):
+        cnt = find_possible_capture(field, side, row3, col3, depth + 1, max_depth, piece_idx, capture)
         for i in range(cnt):
             capture[piece_idx + i, depth + 1] = row2, col2, row3, col3
 
@@ -85,41 +98,32 @@ def find_possible_capture_for_men(field, row, col, depth, max_depth, piece_idx, 
 
 
 @njit()
-def find_field_possible_capture(field, max_capture, capture):
+def find_field_possible_capture(field, side, max_capture, capture):
     piece_idx = 0
     for row in range(field.shape[0]):
         for col in range(field.shape[1]):
-            if field[row, col] == 1:
-                cnt = find_possible_capture_for_men(field, row, col, 0, max_capture, piece_idx, capture)
-                for i in range(cnt):
-                    capture[piece_idx + i, 0] = row, col, max_capture, 0
+            cnt = find_possible_capture(field, side, row, col, 0, max_capture, piece_idx, capture)
+            for i in range(cnt):
+                capture[piece_idx + i, 0] = row, col, max_capture, 0
 
-                piece_idx += cnt
+            piece_idx += cnt
 
     return piece_idx
 
 
 @njit()
-def find_field_possible_moves(field, non_capture):
+def find_field_possible_moves(field, side, non_capture):
     piece_idx = 0
     for row in range(field.shape[0]):
         for col in range(field.shape[1]):
-            if field[row, col] == 1:
-                for i in range(md.shape[0]):
-                    row2 = row + md[i, 0]
-                    col2 = col + md[i, 1]
-                    if not on_board(row2, col2) or field[row2, col2] != 0:
-                        continue
-                    non_capture[piece_idx] = row, col, row2, col2, 0
-                    piece_idx += 1
-
+            for row2, col2 in find_moves_for_piece(field, side, row, col):
+                non_capture[piece_idx] = row, col, row2, col2, 0
+                piece_idx += 1
     return piece_idx
 
 
 @njit()
-def get_possible_moves(input_field: np.ndarray, side: int):
-    field = np.copy(input_field) * side
-
+def get_possible_moves(field: np.ndarray, side: int):
     # dimensions: 0 - idx of sequence, 1 - move idx in sequence, 2 - move (captured_row, captured_col, new_row, new_col)
     # first "move" in every sequence (row, col, capture_depth, promoted)
     capture = np.zeros((20, 16, 4), dtype=np.int8)
@@ -128,13 +132,14 @@ def get_possible_moves(input_field: np.ndarray, side: int):
     non_capture = np.zeros((168, 5), dtype=np.int8)
     non_capture_idx = 0
 
-    max_capture = find_field_capture_max_depth(field)
+    max_capture = find_field_capture_max_depth(field, side)
     if max_capture > 0:
-        captured_idx = find_field_possible_capture(field, max_capture, capture)
+        print(max_capture)
+        captured_idx = find_field_possible_capture(field, side, max_capture, capture)
         for i in range(captured_idx):
             print(capture[i, 0: capture[i, 0, 2] + 1])
     else:
-        non_capture_idx = find_field_possible_moves(field, non_capture)
+        non_capture_idx = find_field_possible_moves(field, side, non_capture)
         for i in range(non_capture_idx):
             print(non_capture[i])
 
@@ -142,17 +147,17 @@ def get_possible_moves(input_field: np.ndarray, side: int):
 
 
 if __name__ == '__main__':
-    start_field = fen_to_field('8/wwwwwwww/wwwwwwww/8/8/bbbbbbbb/bbbbbbbb/8 w')
-    # start_field = np.array([
-    #     [0, 0, 0, 0, 0, 0, 0, 0],
-    #     [-1, -1, -1, -1, -1, -1, -1, -1],
-    #     [-1, -1, -1, -1, -1, -1, -1, -1],
-    #     [0, 0, -1, 0, -1, 0, 0, 0],
-    #     [-1, -1, 0, 0, 0, -1, 0, 0],
-    #     [1, 1, 1, 1, 1, 1, 1, 1],
-    #     [1, 1, 1, 1, 1, 1, 1, 1],
-    #     [0, 0, 0, 0, 0, 0, 0, 0],
-    # ], dtype=np.int8)
+    # start_field = fen_to_field('8/wwwwwwww/wwwwwwww/8/8/bbbbbbbb/bbbbbbbb/8 w')
+    start_field = np.array([
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [-1, -1, -1, -1, -1, -1, -1, -1],
+        [-1, -1, -1, -1, -1, -1, -1, -1],
+        [0, 0, -1, 0, -1, 0, 0, 0],
+        [-1, -1, 0, 0, 0, -1, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+    ], dtype=np.int8)
     print(start_field)
     get_possible_moves(start_field, 1)
 
